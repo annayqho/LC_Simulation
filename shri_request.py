7972231b9265343a34e1b0a5568e16f063d877d9
annayqho <annayqho@gmail.com>
2017-10-18 12:38:59 -0700

    fulfills Shris request

--- shri_request.py
+++ shri_request.py
@@ -1,14 +1,40 @@
 """ For z = 0.2, r-band, generate a SN Ia light curve """
 
 import numpy as np
+import matplotlib.pyplot as plt
 import sncosmo
 from astropy.table import Table
 
-# choose a built-in source
-model = sncosmo.Model(source='hsiao')
 
-# set model parameters
-model.set(z=0.2)
+def simple():
+    # choose a built-in source
+    model = sncosmo.Model(source='hsiao')
 
-# plot
-sncosmo.plot_lc(model=model, bands=['sdssr'])
+    # set model parameters
+    model.set(z=0.2)
+
+    # plot
+    sncosmo.plot_lc(model=model, bands=['sdssr', 'sdssg'])
+    plt.savefig("z02_gr.png")
+    plt.show()
+
+
+def multiple_z():
+    # choose a built-in source
+    models = []
+    npts = 3
+    zs = np.linspace(0, 0.5, npts)
+    for ii in np.arange(npts):
+        model = sncosmo.Model(source='hsiao')
+        model.set(z=zs[ii])
+        models.append(model)
+
+    # plot
+    sncosmo.plot_lc(
+            model=models, bands=['sdssr']*npts, model_label=zs, 
+            color='k')
+    plt.show()
+
+
+if __name__=="__main__":
+    simple()
